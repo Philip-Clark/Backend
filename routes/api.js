402785c914b9cfa00c78ddc1134eeda2181ff287
Philip Clark <ContactPhilipClark@protonmail.com>
2024-05-15 18:50:04 -0400

    add local file save

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,7 @@ const { createCart } = require('../controllers/checkoutController.js');
 const express = require('express');
 const { uploadImage } = require('../controllers/imageUploadController.js');
 const { getProductData } = require('../controllers/productController.js');
+const path = require('path');
 const router = express.Router();
 
 router.post('/checkout', createCart);
@@ -9,5 +10,8 @@ router.post('/checkout', createCart);
 router.post('/image', uploadImage);
 
 router.get('/data', getProductData);
+router.get('/ordered_signs/:name', (req, res) => {
+  res.download(path.join(__dirname, `../ordered_signs/${req.params.name}`));
+});
 
 module.exports = router;
